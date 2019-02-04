6bc5329f80508c078c9b8fab228de81f187821c5
Dilshan karunaratne <nnishadhi95@gmail.com>
2019-02-04 13:47:43 +0530

    header fix

--- src/components/common/HomeNavBar.js
+++ src/components/common/HomeNavBar.js
@@ -1,49 +1,80 @@
-import React from "react";
-import "./homeNavBar.css";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarNav,
-  NavItem,
-  NavLink,
-  NavbarToggler,
-  Collapse,
-  FormInline,
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  Container,
-  Animation
-} from "mdbreact";
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBContainer, MDBView, MDBMask } from 'mdbreact';
+import { BrowserRouter as Router ,Route,Switch } from 'react-router-dom';
 
-class HomeNavBar extends React.Component {
-  state = {
-    collapseID: ""
-  };
 
-  toggleCollapse = collapseID => () =>
-    this.setState(prevState => ({
-      collapseID: prevState.collapseID !== collapseID ? collapseID : ""
-    }));
+class HomeNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
 
   render() {
     return (
-      <Container>
-        <Navbar
-          className="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar"
-          color="red"
-        >
-          <Animation type="bounce" duration="5s">
-            <NavbarBrand>
-              <strong className="white-text">Handallo</strong>
-            </NavbarBrand>
-          </Animation>
-          {/* <NavbarToggler onClick={this.toggleCollapse("navbarCollapse3")} /> */}
-        </Navbar>
-      </Container>
+      <div>
+        <Router>
+          <header>
+            <MDBNavbar color="black" fixed="top" dark expand="md">
+              <MDBContainer>
+                <MDBNavbarBrand href="/">
+                  <h1>Torrid Foods</h1>
+                </MDBNavbarBrand>
+                <MDBNavbarToggler onClick={this.onClick} />
+                <MDBCollapse isOpen={this.state.collapse} navbar>
+                  <MDBNavbarNav left>
+                    <MDBNavItem active>
+                      <MDBNavLink to="/">Home</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem>
+                      <MDBNavLink to="/order">Make Order</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem>
+                      <MDBNavLink to="/adminprofile">Admin Profile</MDBNavLink>
+                    </MDBNavItem> 
+                                 
+                  </MDBNavbarNav>
+
+                  <MDBNavbarNav right>
+
+                  <MDBNavItem >
+                  <form class="form-inline ml-auto">
+                <div class="md-form my-0">
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search"/>
+               </div>
+               <button href="#!" class="btn btn-outline-white btn-md my-0 ml-sm-2" type="submit">Search</button>
+                 </form>
+                  </MDBNavItem>
+
+                 <MDBNavItem>
+                      <MDBNavLink to="/loginadmin">Admin Login</MDBNavLink>
+                    </MDBNavItem>
+                    <MDBNavItem>
+                      <MDBNavLink to="/login">Customer Login</MDBNavLink>
+                    </MDBNavItem>
+
+                  </MDBNavbarNav>
+
+                </MDBCollapse>
+              </MDBContainer>
+            </MDBNavbar>
+
+            </header>
+        </Router>
+
+       
+      </div>
     );
   }
 }
 
-export default HomeNavBar;
+export default HomeNavbar;
